1ab67e26cc199778db83388f98b4544049c3db76
Jayendra Ingale <j.naresh.ingale@accentur.com>
2025-06-12 17:58:37 +0530

    API changes according to tasks

---
+++ final_project/router/bkp_general.js
@@ -0,0 +1,81 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+let doesExist = require("./auth_users.js").doesExist;
+const public_users = express.Router();
+
+
+public_users.post("/register", (req,res) => {
+  //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!doesExist(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  //Write your code here
+  const booksCpy = books
+  return res.send(JSON.stringify(booksCpy, null, 2));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  //Write your code here
+  if (req?.params?.isbn){
+    return res.status(200).json(books[req.params.isbn]);
+  } else {
+    return res.status(300).json({message: "Please give isbn number"});
+  }
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  //Write your code here
+  const author = req?.params?.author
+  const booksByAuthor = []
+  Object.values(books).forEach(book => {
+    if (book.author === author) {
+        booksByAuthor.push(book)
+    }
+  })
+  return res.status(200).json(booksByAuthor);
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  //Write your code here
+  const title = req?.params?.title
+  const booksByTitle = []
+  Object.values(books).forEach(book => {
+    if (book.title === title) {
+        booksByTitle.push(book)
+    }
+  })
+  return res.status(200).json(booksByTitle);
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  //Write your code here
+  if (req?.params?.isbn){
+    return res.status(200).json(books[req.params.isbn].reviews);
+  } else {
+    return res.status(300).json({message: "Please give isbn number"});
+  }
+});
+
+module.exports.general = public_users;
